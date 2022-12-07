b79fdb0ace8c4009a22f808c260341f7a07afa98
altansnl <altansenel@sabanciuniv.edu>
2022-12-07 22:17:57 +0100

    delete old

--- pruning_helpers.py
+++
@@ -1,9 +0,0 @@
-import tensorflow_model_optimization as tfmot
-import tensorflow as tf
-
-# Helper function uses `prune_low_magnitude` to make only the 
-# Dense layers train with pruning.
-def apply_pruning_to_dense(layer):
-  if isinstance(layer, tf.keras.layers.Dense):
-    return tfmot.sparsity.keras.prune_low_magnitude(layer)
-  return layer
